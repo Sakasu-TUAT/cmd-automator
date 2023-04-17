033f8bc0ac19520f8ce4a3433ef08bceae323012
Sota AKASAKA <s214229y@st.go.tuat.ac.jp>
2023-04-17 12:57:49 +0900

    update

--- result.py
+++ result.py
@@ -11,7 +11,7 @@ def main():
         writer.writeheader() #ヘッダー書き込み
 
         #ファイル番号
-        for i in range (0, 2):
+        for i in range (0, 100):
             cmd = ["./a.out", str(i)]
             output = subprocess.check_output(cmd)
             print(output.decode())
@@ -19,6 +19,8 @@ def main():
             time_complexity = int(output.split()[3])
             dist = int(output.split()[-1])
 
+            #表のヘッダー名と合わせること
+            #出力したいデータは自由に変えて
             writer.writerow({'memory': memory, 'timeComplexity': time_complexity, 'dist': dist})
 
 
